814f4cd421dd78f1007d70998e559bb60c8b5062
Raphael Hennessy <raph.hennessy@gmail.com>
2015-11-09 17:43:00 +1100

    Create types.h

---
+++ kernel/inc/types.h
@@ -0,0 +1,21 @@
+//In this file we are declaring all the int types we need for Q OS
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef signed char int8;
+typedef unsigned char uint8;
+
+typedef signed short int16;
+typedef unsigned short uint16;
+
+typedef signed int int32;
+typedef unsigned int uint32;
+
+typedef signed long long int64;
+typedef unsigned long long uint64;
+
+typedef char* string; 
+
+#endif
+
