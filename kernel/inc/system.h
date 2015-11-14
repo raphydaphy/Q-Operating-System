dc304431e0e0bbb48d90e744416cd2170305d4d2
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-14 15:49:10 +1100

    Update system.h
    Update 3.1

--- kernel/inc/system.h
+++ kernel/inc/system.h
@@ -2,17 +2,10 @@
 
 #ifndef SYSTEM_H
 #define SYSTEM_H
-uint8 inportb (uint16 _port)
-{
-    	uint8 rv;
-    	__asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
-    	return rv;
-}
 
-void outportb (uint16 _port, uint8 _data)
-{
-	__asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
-}
+#include "types.h"
+uint8 inportb (uint16 _port);
+
+void outportb (uint16 _port, uint8 _data);
 
 #endif
-
