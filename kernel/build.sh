0a10dd997a95e2a23b377f936f7a828c01ea3e92
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-14 15:45:17 +1100

    Update build.sh
    Updated to support the things I added in Update 3.1

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,17 +1,11 @@
-#Q OS Simple Build Script
-#I will be moving this to makefile when we have more files in the project [eta: 1 week]
-
-#compile the assembly and c code into .bin files
-nasm -f elf32 kernel.asm -o kasm.bin
-gcc -m32 -c kernel.c -o kc.o -ffreestanding
-
-#link the files using the link.ld file into the kernel.bin combined file
-ld -m elf_i386 -T link.ld -o q/boot/kernel.bin kasm.o kc.o
-
-#open the kernel in qemu to test it
+nasm -f elf32 kernel.asm -o kasm.o
+gcc -m32 -c kernel.c -o kc.o -ffreestanding                                                                     
+gcc -m32 -c inc/system.c -o o/system.o -ffreestanding                                                     
+gcc -m32 -c inc/string.c -o o/string.o -ffreestanding                                                     
+gcc -m32 -c inc/screen.c -o o/screen.o -ffreestanding                                                     
+gcc -m32 -c inc/kb.c -o o/kb.o -ffreestanding                                                             
+ld -m elf_i386 -T link.ld -o q/boot/kernel.bin kasm.o kc.o o/system.o o/string.o o/screen.o o/kb.o  
 qemu-system-i386 -kernel q/boot/kernel.bin
-
-#when the qemu session is closed, build the iso image of the kernel - Currently disabled to save time when compiling
-grub-mkrescue -o q.iso q/
+#grub-mkrescue -o q.iso q/
 
 read a
