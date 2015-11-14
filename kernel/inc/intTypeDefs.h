ce51e0eccdce2fc5f16ded7d9a3fe6e5c3c0290b
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-14 18:17:21 +1100

    Create intTypeDefs.h

---
+++ kernel/inc/intTypeDefs.h
@@ -0,0 +1,20 @@
+//In this file we are declaring all the int types we need for Q OS
+#ifndef INTTYPEDEFS_H
+#define INTTYPEDEFS_H
+
+typedef signed char int8;
+typedef unsigned char uint8;
+
+typedef signed short int16;
+typedef unsigned short uint16;
+
+typedef signed int int32;
+typedef unsigned int uint32;
+
+typedef signed long long int64;
+typedef unsigned long long uint64;
+
+typedef char* string; 
+
+#endif
+
