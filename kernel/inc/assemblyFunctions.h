510bd45c0ee39bb54360ab64733b90ca2c35112c
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-14 18:16:54 +1100

    Create assemblyFunctions.h

---
+++ kernel/inc/assemblyFunctions.h
@@ -0,0 +1,12 @@
+//Here we have ASM based functions in C that control port managment for Q OS
+
+#ifndef ASSEMBLYFUNCTIONS_H
+#define ASSEMBLYFUNCTIONS_H
+
+#include "intTypeDefs.h"
+uint8 inportb (uint16 _port);
+
+void outportb (uint16 _port, uint8 _data);
+
+#endif
+
