bfc801a7feac1a7194a3e3b37351ef6a5c725d97
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-14 18:19:50 +1100

    Update build.sh
    Updated to support Update 3.2

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,11 +1,12 @@
-nasm -f elf32 kernel.asm -o kasm.o
-gcc -m32 -c kernel.c -o kc.o -ffreestanding                                                                     
-gcc -m32 -c inc/system.c -o o/system.o -ffreestanding                                                     
-gcc -m32 -c inc/string.c -o o/string.o -ffreestanding                                                     
-gcc -m32 -c inc/screen.c -o o/screen.o -ffreestanding                                                     
-gcc -m32 -c inc/kb.c -o o/kb.o -ffreestanding                                                             
-ld -m elf_i386 -T link.ld -o q/boot/kernel.bin kasm.o kc.o o/system.o o/string.o o/screen.o o/kb.o  
-qemu-system-i386 -kernel q/boot/kernel.bin
-#grub-mkrescue -o q.iso q/
+nasm -f elf32 kernel.asm -o kernel.asm.o
+gcc -m32 -c kernel.c -o kernel.c.o -ffreestanding                                                                     
+gcc -m32 -c inc/assemblyFunctions.c -o o/assemblyFunctions.o -ffreestanding                                                     
+gcc -m32 -c inc/stringUtils.c -o o/stringUtils.o -ffreestanding                                                     
+gcc -m32 -c inc/screenUtils.c -o o/screenUtils.o -ffreestanding                                                     
+gcc -m32 -c inc/kbDetect.c -o o/kbDetect.o -ffreestanding                                                             
+ld -m elf_i386 -T linkAssist.ld -o q/boot/kernel.bin kernel.asm.o kernel.c.o o/assemblyFunctions.o o/stringUtils.o o/screenUtils.o o/kbDetect.o  
+grub-mkrescue -o q.iso q/
+virtualbox
+#qemu-system-i386 -kernel q/boot/kernel.bin			#qemu dosen't seem to work with Update 3.1+ versions of Q OS
 
 read a
