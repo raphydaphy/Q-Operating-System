83567d72d9fd591eb94d1bb6a99a06fc051116e0
Paul <plankp@outlook.com>
2015-11-16 20:23:57 -0500

    Update byteUtils.c

--- kernel/inc/byteUtils.c
+++ kernel/inc/byteUtils.c
@@ -1,18 +1,29 @@
 #include "byteUtils.h"
 
 // Copy len bytes from src to dest
-void memcpy(uint8* dest, const uint8* src, uint32 len)
+void* memcpy(void* dest, const void* src, uint32 len)
 {
-    const uint8* sp = (const uint8*)src;
-    uint8* dp = (uint8*)dest;
+    string sp = (string) src;
+    string dp = (string) dest;
     for(; len != 0; len--)
         *dp++ = *sp++;
+    return dest;
 }
 
 // Write len copies of val into dest.
-void memset(uint8* dest, uint8 val, uint32 len)
+void* memset(void* dest, char val, uint32 len)
 {
-    uint8* temp = (uint8*)dest;
-    for ( ; len != 0; len--)
+    string temp = (string) dest;
+    for (; len != 0; len--)
         *temp++ = val;
+    return dest;
+}
+
+// Like memset but with uint16
+uint16* memsetw(uint16* dest, uint16 val, uint32 count)
+{
+    uint16* temp = (uint16 *)dest;
+    for( ; count != 0; count--)
+        *temp++ = val;
+    return dest;
 }
