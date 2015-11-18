b29716ca91b8cba40e614747fa50be011c606888
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2015-11-18 01:44:50 +0100

    Removed autogenerated .d filed and added it to .gitignore

--- kernel/inc/byteUtils.d
+++
@@ -1,6 +0,0 @@
-kernel/inc/byteUtils.o: kernel/inc/byteUtils.c kernel/inc/byteUtils.h \
- kernel/inc/intTypeDefs.h
-
-kernel/inc/byteUtils.h:
-
-kernel/inc/intTypeDefs.h:
--- kernel/inc/fs.d
+++
@@ -1,5 +0,0 @@
-kernel/inc/fs.o: kernel/inc/fs.c kernel/inc/fs.h kernel/inc/intTypeDefs.h
-
-kernel/inc/fs.h:
-
-kernel/inc/intTypeDefs.h:
