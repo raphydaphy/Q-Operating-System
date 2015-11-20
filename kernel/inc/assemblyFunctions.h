abd3d8da15a2d9edeb08bc647ccb9c67b986d5c1
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-21 10:18:51 +1100

    fixed reboot

--- kernel/inc/assemblyFunctions.h
+++ kernel/inc/assemblyFunctions.h
@@ -4,9 +4,10 @@
 #define ASSEMBLYFUNCTIONS_H
 
 #include "intTypeDefs.h"
-uint8 inportb (uint16 _port);
 
+uint8 inportb (uint16 _port);
 void outportb (uint16 _port, uint8 _data);
+void halt();
+void reboot();
 
 #endif
-
