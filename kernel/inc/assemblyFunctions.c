850a7588612c7d58a4d701682d5500d715923087
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-21 10:11:17 +1100

    Close #20, added reboot & half function

--- kernel/inc/assemblyFunctions.c
+++ kernel/inc/assemblyFunctions.c
@@ -10,3 +10,18 @@ void outportb (uint16 _port, uint8 _data)
 {
 	__asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
+
+void halt()
+{
+    loop:
+    __asm__ __volatile__ ("hlt");
+    goto loop;
+}
+void reboot()
+{
+    uint8 good = 0x02;
+    while (good & 0x02)
+        good = inportb(0x64);
+    outportb(0x64, 0xFE);
+    halt();
+}
