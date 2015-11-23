d7c4dfdbb2b9fdc1699faa975eba89a3c1e3f189
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-23 21:41:05 +1100

    made new apps folder for shell command functions

---
+++ kernel/apps/execute.h
@@ -0,0 +1,14 @@
+#ifndef EXECUTE_H
+#define EXECUTE_H
+
+#ifndef EXECUTESIZE
+#define EXECUTESIZE 1024
+#endif
+
+// create input holders for the execute program
+char executeContents[EXECUTESIZE];
+
+// declare the main execute() function for when "execute" is written into the command line
+void execute();
+
+#endif
