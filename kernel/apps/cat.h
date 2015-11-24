5cf2621c146e7725556a2b9402bf8cac91bd67e6
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-24 20:49:50 +1100

    Moved catFile here from kernelFunctions.c

---
+++ kernel/apps/cat.h
@@ -0,0 +1,12 @@
+#ifndef CAT_H
+#define CAT_H
+
+#include "../inc/intTypeDefs.h"
+#include "../inc/screenUtils.h"
+#include "../inc/kbDetect.h"
+#include "../inc/kernelFunctions.h"
+#include "../inc/fs.h"
+
+void cat(fs_node_t*);
+
+#endif
