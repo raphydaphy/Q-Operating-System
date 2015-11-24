6f6f0c00031f18df2ecfadeecdc1d436948146c5
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-24 20:04:03 +1100

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -26,7 +26,7 @@ uint32 findInitrd(struct multiboot*);
 int kmain(struct multiboot* mboot_ptr)
 {
 	clearScreen();
-	
+
 	init_descriptor_tables();
 	__asm__ __volatile__("sti");
 
@@ -35,24 +35,24 @@ int kmain(struct multiboot* mboot_ptr)
 
 	// Initialize the initial ramdisk, and set it as the filesystem root.
 	fs_root = initialize_initrd(initrd_location);
-	
-    	printIntro();
 
-    	println(PRO_TIP, 0x0F);
-    	kbHelp();
-	
-    	launchShell();
-	
+	printIntro();
+
+	println(PRO_TIP, 0x0F);
+	kbHelp();
+
+	launchShell();
+
 	return 0;
 }
 
 uint32 findInitrd(struct multiboot* mboot_ptr)
 {
-    // Find the location of our initial ramdisk.
-    ASSERT(mboot_ptr->mods_count > 0);
-    uint32 initrd_location = *((uint32*)mboot_ptr->mods_addr);
-    uint32 initrd_end = *(uint32*)(mboot_ptr->mods_addr+4);
-    // Don't trample our module with placement accesses, please!
-    placement_address = initrd_end;
-    return initrd_location;
+	// Find the location of our initial ramdisk.
+	ASSERT(mboot_ptr->mods_count > 0);
+	uint32 initrd_location = *((uint32*)mboot_ptr->mods_addr);
+	uint32 initrd_end = *(uint32*)(mboot_ptr->mods_addr+4);
+	// Don't trample our module with placement accesses, please!
+	placement_address = initrd_end;
+	return initrd_location;
 }
