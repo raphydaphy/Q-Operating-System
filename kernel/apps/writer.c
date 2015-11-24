695ca7ada1771b5ac659103acd26114cf7de85d7
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-25 07:47:55 +1100

    Now with proper command arguments

--- kernel/apps/writer.c
+++ kernel/apps/writer.c
@@ -1,6 +1,7 @@
 #include "writer.h"
 
 #include "../inc/intTypeDefs.h"
+#include "../inc/stringUtils.h"
 #include "../inc/screenUtils.h"
 #include "../inc/kbDetect.h"
 
@@ -8,23 +9,30 @@
 #define WRITERSIZE 1024
 #endif
 
-void writer()
-{
-	// this is called when someone types "writer" on the command line
-
-	clearScreen();
-	print("================================================================================", 0x3F);
-	print("                      Q OS Text Editor Version 0.2                              ", 0x3F);
-	print("================================================================================", 0x3F);
-	writing = true;
-	readStr(writerContents, WRITERSIZE);
-	writing = false;
-}
-
 void writerHelp()
 {
 	newline();
 	print("Showing help for writer:",0x0F);
 	newline();
 	print("[HELP SECTION HERE]",0x0F);
+}
+
+void writer(string args)
+{
+	if (strEql(args," -h"))
+	{
+		writerHelp();
+	}
+	else
+	{
+		// this is called when someone types "writer" on the command line
+
+		clearScreen();
+		print("================================================================================", 0x3F);
+		print("                      Q OS Text Editor Version 0.2                              ", 0x3F);
+		print("================================================================================", 0x3F);
+		writing = true;
+		readStr(writerContents, WRITERSIZE);
+		writing = false;
+	}
 }
