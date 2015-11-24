88c51cb4c583049ece9ebaebb2c3a4846186b40e
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-25 07:45:56 +1100

    Now supporting the system command

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,116 +1,61 @@
-#include "../multiboot.h"
+#include "multiboot.h"
 
-#include "kernelFunctions.h"
-#include "fs.h"
-#include "timer.h"
-#include "error.h"
-#include "paging.h"
-#include "initrd.h"
-#include "kbDetect.h"
-#include "descriptorTables.h"
-#include "assemblyFunctions.h"
+// Q Libraries
+#include "inc/kernelFunctions.h"
+#include "inc/fs.h"
+#include "inc/timer.h"
+#include "inc/error.h"
+#include "inc/paging.h"
+#include "inc/initrd.h"
+#include "inc/kbDetect.h"
+#include "inc/descriptorTables.h"
+#include "inc/assemblyFunctions.h"
 
+// Q Applications
+#include "apps/execute.h"
+#include "apps/calc.h"
+#include "apps/writer.h"
+#include "apps/cat.h"
+#include "apps/files.h"
+#include "apps/system.h"
 
+extern uint32 placement_address;
 
-void printIntro(){
-   	print("================================================================================", 0x3F);
-   	print("                             Welcome to Q OS                                    ", 0x3F);
-    print("================================================================================", 0x3F);
+#define COMMAND_HELP "\nWorking Commands in Q OS: \nwriter\nclear\nexecute\nhi\nskip (the no action)\nfiles\ncat\nreboot\ncalc"
+#define PRO_TIP "\nTip: If enter key does not work, it might mean that the input is too long"
+
+uint32 findInitrd(struct multiboot*);
+
+int kmain(struct multiboot* mboot_ptr)
+{
+	clearScreen();
+
+	init_descriptor_tables();
+	__asm__ __volatile__("sti");
+
+	uint32 initrd_location = findInitrd(mboot_ptr);
+	initialize_paging();
+
+	// Initialize the initial ramdisk, and set it as the filesystem root.
+	fs_root = initialize_initrd(initrd_location);
+
+	printIntro();
+
+	println(PRO_TIP, 0x0F);
+	kbHelp();
+
+	launchShell();
+
+	return 0;
 }
 
-void launchShell() {
-    //allocate some memory for command string buffer. 1kB should be enough for now
-    const int bufSize = 128;
-    char bufStr[bufSize];//Store sanitized user command (no arguments)
-    char rawCommand[bufSize];//Gets user raw command from command line
-    char arguments[bufSize/2][bufSize/2];//Store command arguments
-    int fs = 1;//First space (first word means actual command)
-    int ay = -1;//Y location for arguments
-    int ax = 0;//X location for argumetns
-
-    //prepare variable
-    for(int i = 0; i < bufSize; ++i){
-	bufStr[i] = 0;
-    }
-    for(int y = 0; y < bufSize; ++y){
-	for(int x = 0; x < bufSize; ++x){
-		arguments[y][x] = 0;
-    	}
-    }
-
-    #define TIP print("\nTip: If enter key does not work, it might mean that the input is too long",0x0F);
-    #define HELP print("\nWorking Commands in Q OS: \nwriter\nclear\nexecute\nhi\nskip (the no action)\nfiles\ncat\nreboot\ncalc", 0x0F);
-    #define BIGHELP kbHelp(); TIP; HELP;
-    #define SYSTEMMAN print("\nSystem Utility\nCurrently only able to reboot and halt system.\nHard shutdown to power off.\n\nMenu:\n1. [reboot]\n2. [halt]\n3. [shutdown]\n> ", 0x0F); readStr(bufStr, bufSize); if(strEql(bufStr, "reboot")) { reboot(); } else if(strEql(bufStr, "halt")) { halt(); } else if(strEql(bufStr, "shutdown")) { shutdown(); }
-    #define SAYHI print("\nHello!", 0x3F);
-    #define CATFILE print("\nFile Name>  ", 0x0F); readStr(bufStr, bufSize); ASSERT(strlength(bufStr) < MAX_FNAME_LEN); cat(finddir_fs(fs_root, bufStr));
-    #define SWITCHDIR print("\nThe specified directory was not found ", 0x0F);
-    #define CALCULATE calc(arguments[0]);
-    #define CLEAR clearScreen(); cursorX = 0; cursorY = 0; updateCursor();
-    #define BIGCLEAR CLEAR; printIntro();
-    #define MKDIR print("\nThis Command is Reserved for when we have a FAT32 or better FileSystem...", 0x3F);
-    #define RMFILE print("\nThis Command is Reserved for when we have a FAT32 or better FileSystem...", 0x3F);
-    #define SKIP
-    #define CMDNOTFOUND print("\n", 0x0F); print(bufStr, 0x0F); print(": Command Not Found ", 0x0F);
-
-    while (true)
-    {
-        print("\nQ-Kernel>  ", 0x08);
-        typingCmd = true;
-        newCmd = true;
-        readStr(rawCommand, bufSize);
-        typingCmd = false;
-
-    	for(int i = 0; i < bufSize; ++i){
-    	    bufStr[i] = 0;
-    	}
-    	for(int y = 0; y < bufSize; ++y){
-                for(int x = 0; x < bufSize; ++x){
-    		arguments[y][x] = 0;
-    	    }
-    	}
-    	fs = 1;
-            ay = -1;
-            ax = 0;
-
-    	//Sanitize raw input. Move first word to bufStr and move the rest of the word to arguments
-    	for(int i = 0; i < bufSize; ++i){
-    	    if(rawCommand[i] != 0 || rawCommand[i] != 10){
-    		if(fs == 1)
-    		    bufStr[i] = rawCommand[i];
-    		if(i < bufSize && rawCommand[i+1] == 32){
-    		    fs = 0;
-    		    ay++;
-    		    ax = 0;
-    		}
-
-    		else if(fs == 0){
-    		    arguments[ay][ax] = rawCommand[i];
-    		    ax++;
-    		}
-    	    }else{
-    	    	break;
-    	    }
-    	}
-
-        if (strEql(strTrim(bufStr), ""))        {   HELP;             }
-        else if(strEql(bufStr, "help"))         {   BIGHELP;          }
-        else if(strEql(bufStr, "system"))       {   SYSTEMMAN;        }
-        else if(strEql(bufStr, "skip"))         {   SKIP;           }
-        else if(strEql(bufStr, "hi"))           {   SAYHI;            }
-        else if(strEql(bufStr, "files"))        {   files();          }
-        else if(strEql(bufStr, "cat"))          {   CATFILE;          }
-        else if(strEql(bufStr,"execute"))       {   execute();        }
-        else if(strEql(bufStr,"switch"))        {   SWITCHDIR;        }
-        else if(strEql(bufStr,"writer"))        {   writer();         }
-    	else if(strEql(bufStr, "writer -h"))    {   writerHelp();     }
-    	else if(strEql(bufStr, "calc"))         {   CALCULATE;        }
-        else if(strEql(bufStr, "calc -h"))      {   calcHelp();       }
-        else if(strEql(bufStr, "clear"))        {   CLEAR;            }
-        else if(strEql(bufStr, "clear -i"))     {   BIGCLEAR;         }
-        else if(strEql(bufStr, "newdir"))       {   MKDIR;            }
-        else if(strEql(bufStr, "erase"))        {   RMFILE;           }
-        else                                    {   CMDNOTFOUND;      }
-        newline();
-    }
+uint32 findInitrd(struct multiboot* mboot_ptr)
+{
+	// Find the location of our initial ramdisk.
+	ASSERT(mboot_ptr->mods_count > 0);
+	uint32 initrd_location = *((uint32*)mboot_ptr->mods_addr);
+	uint32 initrd_end = *(uint32*)(mboot_ptr->mods_addr+4);
+	// Don't trample our module with placement accesses, please!
+	placement_address = initrd_end;
+	return initrd_location;
 }
