fa132a98010dd581c0841df49b3172aeed124744
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-24 17:57:09 +1100

    Header file for new kernelFunctions.c

---
+++ kernel/inc/kernelFunctions.h
@@ -0,0 +1,20 @@
+#ifndef KERNELFUNCTIONS_H
+#define KERNELFUNCTIONS_H
+
+#include "../multiboot.h"
+#include "kernelFunctions.h"
+#include "fs.h"
+#include "timer.h"
+#include "error.h"
+#include "paging.h"
+#include "initrd.h"
+#include "kbDetect.h"
+#include "descriptorTables.h"
+#include "assemblyFunctions.h"
+
+void printInfo();
+void catFile(fs_node_t*);
+void listTree();
+void kbHelp();
+
+#endif
