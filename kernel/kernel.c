2456b9ce5b86daba7707667d83f1786d929853ac
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-24 17:56:38 +1100

    moved most kernel functions to kernelFunctions.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,7 @@
 #include "multiboot.h"
 
 // Q Libraries
+#include "inc/kernelFunctions.h"
 #include "inc/fs.h"
 #include "inc/timer.h"
 #include "inc/error.h"
@@ -13,81 +14,38 @@
 // Q Applications
 #include "apps/calc.h"
 #include "apps/writer.h"
+#include "apps/execute.h"
 
 extern uint32 placement_address;
 
 #define COMMAND_HELP "\nWorking Commands in Q OS: \nwriter\nclear\nexecute\nhi\nskip (the no action)\nfiles\ncat\nreboot\ncalc"
 #define PRO_TIP "\nTip: If enter key does not work, it might mean that the input is too long"
 
-void listTree();
-void launchShell();
-void catFile(fs_node_t*);
-uint32 findInitrd(struct multiboot*);
 
-void printIntro(){
-    print("================================================================================", 0x3F);
-    print("                             Welcome to Q OS                                    ", 0x3F);
-    print("================================================================================", 0x3F);
-}
+void launchShell();
+uint32 findInitrd(struct multiboot*);
 
 int kmain(struct multiboot* mboot_ptr)
 {
-    clearScreen();
-    init_descriptor_tables();
-    __asm__ __volatile__("sti");
+	clearScreen();
+	
+	init_descriptor_tables();
+	__asm__ __volatile__("sti");
 
-    uint32 initrd_location = findInitrd(mboot_ptr);
-    initialize_paging();
+	uint32 initrd_location = findInitrd(mboot_ptr);
+	initialize_paging();
 
-    // Initialize the initial ramdisk, and set it as the filesystem root.
-    fs_root = initialize_initrd(initrd_location);
+	// Initialize the initial ramdisk, and set it as the filesystem root.
+	fs_root = initialize_initrd(initrd_location);
+	
+    	printIntro();
 
-    printIntro();
-
-    println(PRO_TIP, 0x0F);
-    kbHelp();
-
-    launchShell();
-    return 0;
-}
-
-void catFile(fs_node_t* fsnode)
-{
-    newline();
-    if ((fsnode->flags & 0x7) == FS_FILE)
-    {
-        const uint32 rbuff = fsnode->length;
-        char buf[rbuff];
-        uint32 sz = read_fs(fsnode, 0, rbuff, (uint8*) buf);
-        uint32 j;
-        for (j = 0; j < sz; j++)
-            printch(buf[j], 0x0F);
-        newline();
-    }
-}
-
-void listTree() {
-    // list the contents of
-    int i = 0;
-    struct dirent *node = 0;
-    while ((node = readdir_fs(fs_root, i)) != 0)
-    {
-        fs_node_t *fsnode = finddir_fs(fs_root, node->name);
-
-        if ((fsnode->flags & 0x7) == FS_DIRECTORY)
-        {
-            print("dir \t", 0x0F);
-            print(node->name, 0x0F);
-            printch('/', 0x0F);
-        }
-        else
-        {
-            print("file\t", 0x0F);
-            print(node->name, 0x0F);
-        }
-        newline();
-        i++;
-    }
+    	println(PRO_TIP, 0x0F);
+    	kbHelp();
+	
+    	launchShell();
+	
+	return 0;
 }
 
 uint32 findInitrd(struct multiboot* mboot_ptr)
@@ -101,26 +59,11 @@ uint32 findInitrd(struct multiboot* mboot_ptr)
     return initrd_location;
 }
 
-void kbHelp()
-{
-    print("\nKeybindings in Q OS:", 0x0F);
-    print("\n\tCtrl-b -> left", 0x0F);
-    print("\n\tCtrl-f -> right", 0x0F);
-    print("\n\tCtrl-a -> home", 0x0F);
-    print("\n\tCtrl-p -> up", 0x0F);
-    print("\n\tCtrl-n -> down", 0x0F);
-    print("\n\tCtrl-z -> quit", 0x0F);
-    println("\n\tCtrl-l -> clear", 0x0F);
-}
-
 void launchShell() {
     //allocate some memory for command string buffer. 1kB should be enough for now
     const int bufSize = 128;
     char bufStr[bufSize];
 
-    
-
-   
     while (true)
     {
         print("\nQ-Kernel>  ", 0x08);
