f8b357e36d5e668df65cd828969f4e1f3e37d1ea
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-26 07:56:57 +1100

    Removed a strange &...

--- kernel/inc/kernelFunctions.c
+++ kernel/inc/kernelFunctions.c
@@ -43,7 +43,7 @@ void launchShell() {
     #define SAYHI print("\nHello!", 0x3F);
     #define CATFILE print("\nFile Name>  ", 0x0F); readStr(bufStr, bufSize); ASSERT(strlength(bufStr) < MAX_FNAME_LEN); cat(finddir_fs(fs_root, bufStr));
     #define SWITCHDIR print("\nThe specified directory was not found ", 0x0F);
-    #define CALCULATE calc(&arguments);
+    #define CALCULATE calc(arguments);
     #define BIGCLEAR clearScreen(); printIntro();
     #define MKDIR print("\nThis Command is Reserved for when we have a FAT32 or better FileSystem...", 0x3F);
     #define RMFILE print("\nThis Command is Reserved for when we have a FAT32 or better FileSystem...", 0x3F);
