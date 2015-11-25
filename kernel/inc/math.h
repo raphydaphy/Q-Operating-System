780a10ca5c31e05b5ccbddf3fc69e800cb350c6a
krisna <krisna@mail.krisnaslife.cu.cc>
2015-11-25 13:41:13 +0700

    Updated manual.txt to include system command arguements and calculator arguments. Added math.h and updated calculator.
    Calculator accepts more math function (WIP) and math.h needs
    lots of work.

---
+++ kernel/inc/math.h
@@ -0,0 +1,17 @@
+#ifndef MATH_H
+#define MATH_H
+
+#ifndef PI
+#define PI 3.141592653589793238462643383279502884
+#endif
+
+//Square root function
+double sqrt(double a);
+
+//power of ten
+double powerOfTen(int num);
+
+//power of n
+//double pow(double num, double);
+
+#endif
