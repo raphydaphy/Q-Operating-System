c0afab12743b8023bdc2fbaf1376bd71fe2dc419
Plankp <plankp@outlook.com>
2015-11-28 15:26:15 -0500

    Added itos64 and diff between lower and upper

--- kernel/inc/charUtils.c
+++ kernel/inc/charUtils.c
@@ -24,22 +24,30 @@ bool isupper(char ch) {
 
 int ctoi(char c)
 {
-    if (isnum(c)) {
+    if (isnum(c))
         return c - '0';
-    } else {
-        c = toLowerC(c);
-        if (islower(c)) {
-            return c - 'a';
-        } else return -1;
-    }
+    else if (islower(c))
+        return c - 'a' + 10;
+    else if (isupper(c))
+        return c - 'A' + 36;
+    else if (c == '+')
+        return 62;
+    else if (c == '/')
+        return 63;
+    return -1;
 }
 
 char itoc(int i) {
-    if (IN_RANGE(i, 0, 9)) {
+    if (IN_RANGE(i, 0, 9))
         return i + '0';
-    } else if (IN_RANGE(i, 10, 35)) {
+    else if (IN_RANGE(i, 10, 35))
         return i - 10 + 'a'; // Use lowercase for hex digits
-    }
+    else if (IN_RANGE(i, 36, 61))
+        return i - 36 + 'A'; // Use UPPERCASE for higher encoding digits
+    else if (i == 62)
+        return '+';
+    else if (i == 63)
+        return '/';
     return '\0';
 }
 
