b221cb2390fca6ab5d2c99f4f28802ebb2f3eeab
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-30 14:09:02 +1100

    yay, me is a thing now

---
+++ kernel/apps/me.h
@@ -0,0 +1,9 @@
+#ifndef ME_H
+#define ME_H
+
+// me is a random personal assistant
+// @raph will make all the stuff for it
+// basically just don't modify this files
+// or you will probably get blown up :)
+
+#endif
