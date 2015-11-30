64a215aec2f0c14f5a8ff0a794845ab4f2b06ca5
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-30 15:00:41 +1100

    Me now automatically runs setup on first call

--- kernel/apps/me.c
+++ kernel/apps/me.c
@@ -1,13 +1,16 @@
 #include "me.h"
 
+bool hasSetup = false;
+
 void me(string args) {
-    if (strEql(splitArg(args, 1), "setup"))
+    if (strEql(splitArg(args, 1), "setup") || !hasSetup)
     {
-        print("Welcome to Me.",0x03);
+        print("\nWelcome to Me.",0x03);
         print("\nMe is the worlds first truly rubbish personal assistant.",0x03);
         print("\nTo use me, simply ask it a question.",0x03);
         print("\nFor example, type 'me what is the time?'",0x03);
         print("\nTo start using Me, please enter some basic information below when prompted.",0x03);
+        newline();
 
         newline();
         print("What is your name: ",0x02);
@@ -44,6 +47,13 @@ void me(string args) {
         newline();
         print("What is the zip code in your area: ",0x02);
         readStr(zip,128);
+
+        newline();
+        hasSetup = "true";
+        print("Me is now ready to use!",0x03);
+
+        newline();
+        print("When Me dosen't know information, it can ask you for help.",0x03);
     }
     else
     {
@@ -51,7 +61,22 @@ void me(string args) {
         newline();
         print(splitArg(args, 1),0x0F);
         newline();
-        print("\n",0x0F);
         print(splitArg(args, 2),0x0F);
+        newline();
+        print(splitArg(args, 3),0x0F);
+        newline();
+        print(splitArg(args, 4),0x0F);
+        newline();
+        print(splitArg(args, 5),0x0F);
+        newline();
+        print(splitArg(args, 6),0x0F);
+        newline();
+        print(splitArg(args, 7),0x0F);
+        newline();
+        print(splitArg(args, 8),0x0F);
+        newline();
+        print(splitArg(args, 9),0x0F);
+        newline();
+        print(splitArg(args, 10),0x0F);
     }
 }
