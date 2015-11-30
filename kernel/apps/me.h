54dfed47a0035d5f58adb3600f225987402f908d
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-30 16:07:38 +1100

    Fixed words array datatype definition

--- kernel/apps/me.h
+++ kernel/apps/me.h
@@ -7,7 +7,20 @@
 #include "../inc/screenUtils.h"
 
 // An array to store each word that you enter
-char* words[9];
+char *words[9];
+
+// Variables for each word you enter
+string word1;
+string word2;
+string word3;
+string word4;
+string word5;
+string word6;
+string word7;
+string word8;
+string word9;
+string word10;
+
 
 // Some variables to store basic information about our user
 string name;
@@ -21,6 +34,9 @@ string country;
 string state;
 string city;
 string zip;
+
+// random counter integers needed in various parts of me.c
+//int tmp;
 
 void me(string args);
 
