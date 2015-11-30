0370968eb0636222e3bf28774bd0dee3c1abc8b7
Raph Hennessy <raph.hennessy@gmail.com>
2015-11-30 14:12:01 +1100

    declared me function

--- kernel/apps/me.h
+++ kernel/apps/me.h
@@ -1,9 +1,16 @@
 #ifndef ME_H
 #define ME_H
 
+#include "../inc/intTypeDefs.h"
+#include "../inc/stringUtils.h"
+#include "../inc/screenUtils.h"
+#include "../inc/kbDetect.h"
+
 // me is a random personal assistant
 // @raph will make all the stuff for it
 // basically just don't modify this files
 // or you will probably get blown up :)
+
+void me(string);
 
 #endif
