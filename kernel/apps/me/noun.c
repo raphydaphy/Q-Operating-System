b99b32953396f64bb875992aab6e561adcff72dc
Raph Hennessy <raph.hennessy@gmail.com>
2015-12-01 12:22:06 +1100

    Started making noun lists

---
+++ kernel/apps/me/noun.c
@@ -0,0 +1,127 @@
+#include "noun.h"
+
+char *noun_act[] = {
+    "abort",
+    "kindness",
+    "feat",
+    "update",
+    "playing",
+    "recovery",
+    "going",
+    "entree",
+    "getaway",
+    "winning",
+};
+
+char *noun_animal[] {
+    "darter",
+    "varmint",
+    "peeper",
+    "stocker",
+    "migrator",
+    "polyploid",
+    "diploid",
+    "puppy",
+    "animalcule",
+    "mutant",
+};
+
+char *noun_artifact[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_attribute[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_body[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_cognition[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_communication[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_event[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_feeling[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_food[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_group[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_location[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_motive[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_object[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_person[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_quantity[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_relation[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_shape[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_state[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_substance[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_time[] {
+    "Thing",
+    "Another Thing"
+};
+
+char *noun_tops[] {
+    "Thing",
+    "Another Thing"
+};
