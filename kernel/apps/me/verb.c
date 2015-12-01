6b3c1edc940ddb929d8244a34db0506cfb66cda1
Raph Hennessy <raph.hennessy@gmail.com>
2015-12-01 12:32:52 +1100

    started big verb list

---
+++ kernel/apps/me/verb.c
@@ -0,0 +1,81 @@
+#include "verb.h"
+
+char *verb_body[] = {
+    "",
+    ""
+}
+
+char *verb_change[] = {
+    "",
+    ""
+}
+
+char *verb_cognition[] = {
+    "",
+    ""
+}
+
+char *verb_communicaiton[] = {
+    "",
+    ""
+}
+
+char *verb_competition[] = {
+    "",
+    ""
+}
+
+char *verb_consumption[] = {
+    "",
+    ""
+}
+
+char *verb_contact[] = {
+    "",
+    ""
+}
+
+char *verb_creation[] = {
+    "",
+    ""
+}
+
+char *verb_emotion[] = {
+    "",
+    ""
+}
+
+char *verb_Framestext[] = {
+    "",
+    ""
+}
+
+char *verb_motion[] = {
+    "",
+    ""
+}
+
+char *verb_perception[] = {
+    "",
+    ""
+}
+
+char *verb_possession[] = {
+    "",
+    ""
+}
+
+char *verb_social[] = {
+    "",
+    ""
+}
+
+char *verb_stative[] = {
+    "",
+    ""
+}
+
+char *verb_weather[] = {
+    "",
+    ""
+}
