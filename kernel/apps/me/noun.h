c0da5b57973d98ea673ae14f2cd364409ad76c35
Raph Hennessy <raph.hennessy@gmail.com>
2015-12-01 20:40:19 +1100

    initiated all the arrays with a value of 10

--- kernel/apps/me/noun.h
+++ kernel/apps/me/noun.h
@@ -1,27 +1,27 @@
 #ifndef NOUN_H
 #define NOUN_H
 
-extern char *noun_act[];
-extern char *noun_animal[];
-extern char *noun_artifact[];
-extern char *noun_attribute[];
-extern char *noun_body[];
-extern char *noun_cognition[];
-extern char *noun_communication[];
-extern char *noun_event[];
-extern char *noun_feeling[];
-extern char *noun_food[];
-extern char *noun_group[];
-extern char *noun_location[];
-extern char *noun_motive[];
-extern char *noun_object[];
-extern char *noun_person[];
-extern char *noun_quantity[];
-extern char *noun_relation[];
-extern char *noun_shape[];
-extern char *noun_state[];
-extern char *noun_substance[];
-extern char *noun_time[];
-extern char *noun_tops[];
+extern char *noun_act[10];
+extern char *noun_animal[10];
+extern char *noun_artifact[10];
+extern char *noun_attribute[10];
+extern char *noun_body[10];
+extern char *noun_cognition[10];
+extern char *noun_communication[10];
+extern char *noun_event[10];
+extern char *noun_feeling[10];
+extern char *noun_food[10];
+extern char *noun_group[10];
+extern char *noun_location[10];
+extern char *noun_motive[10];
+extern char *noun_object[10];
+extern char *noun_person[10];
+extern char *noun_quantity[10];
+extern char *noun_relation[10];
+extern char *noun_shape[10];
+extern char *noun_state[10];
+extern char *noun_substance[10];
+extern char *noun_time[10];
+extern char *noun_tops[10];
 
 #endif
