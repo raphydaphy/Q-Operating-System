cd0869bb2abac1b36e430ce6aab5bcfb1cd205f4
Raph Hennessy <raph.hennessy@gmail.com>
2015-12-01 15:38:43 +1100

    Started making word sort function (to word type)

---
+++ kernel/apps/me/sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+double sort(string word)
+{
+    // Here are the basic category numbers, for more information look on the Q OS Wiki for Me
+    // Link: https://github.com/raphydaphy/Q-Operating-System/wiki/Me
+
+    // 0 = unknown                      The kind of this word is unknown or unspecified
+    // 1 = noun                         A noun is a person, place, thing, quality or act word
+    // 2 = verb                         Verbs are action/existence words that say what a noun does
+    // 3 = adjective                    An adjective is a word that describes a noun
+    // 4 = Adverb                       An adverb descries a verb, adjective or adverb. It often ends in 'ly'
+    // 5 = Interjection                 An interjection is an outcry/ sudden utterance. Usally the start of a sentence
+    // 6 = Preposition                  A preposition describes the relationship between a noun and another noun (can also be verb/adverb)
+    // 7 = Conjunction                  A conjunction is a word that joins together multiple thoughts
+    // 8 = Pronoun                      A pronoun replaces a noun/noun phase that is understood from context
+
+    int type = 0;
+
+    return type;
+}
