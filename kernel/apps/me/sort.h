86dcbf31586f73dbcb9aa3de290db4f9f86821c7
Raph Hennessy <raph.hennessy@gmail.com>
2015-12-01 20:53:01 +1100

    added comments about what sort() does

--- kernel/apps/me/sort.h
+++ kernel/apps/me/sort.h
@@ -10,6 +10,10 @@
 #include "noun.h"
 #include "verb.h"
 
+// give this function a word and it will potentially tell you what kind of word it is in detail
+// for example, "puppy" returns 1.2 which indicates a "noun" about "animals"
+// for more information on all the return values for this function have a look at the wiki
+// link: https://github.com/raphydaphy/Q-Operating-System/wiki/Me
 double sort(char*);
 
 #endif
