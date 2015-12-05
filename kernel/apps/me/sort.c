d32f16d671773c18d24686cbdd66024a9b853851
Raph Hennessy <raph.hennessy@gmail.com>
2015-12-05 14:13:18 +1100

    change filenames in me.c to match Logical Extensions

--- kernel/apps/me/sort.c
+++ kernel/apps/me/sort.c
@@ -70,51 +70,51 @@ string sort(char* word)
     word = toUpper(word);
 
     // try to find the sub-category of the word assuming it is a noun
-    if (findInDictionary("me/noun/act.txt",word))               { return words[1][1];   }
-    if (findInDictionary("me/noun/animal.txt",word))            { return words[1][2];   }
-    if (findInDictionary("me/noun/artifact.txt",word))          { return words[1][3];   }
-    if (findInDictionary("me/noun/attribute.txt",word))         { return words[1][4];   }
-    if (findInDictionary("me/noun/body.txt",word))              { return words[1][5];   }
-    if (findInDictionary("me/noun/cognition.txt",word))         { return words[1][6];   }
-    if (findInDictionary("me/noun/communication.txt",word))     { return words[1][7];   }
-    if (findInDictionary("me/noun/event.txt",word))             { return words[1][8];   }
-    if (findInDictionary("me/noun/feeling.txt",word))           { return words[1][9];   }
-    if (findInDictionary("me/noun/food.txt",word))              { return words[1][10];  }
-    if (findInDictionary("me/noun/group.txt",word))             { return words[1][11];  }
-    if (findInDictionary("me/noun/location.txt",word))          { return words[1][12];  }
-    if (findInDictionary("me/noun/motive.txt",word))            { return words[1][13];  }
-    if (findInDictionary("me/noun/object.txt",word))            { return words[1][14];  }
-    if (findInDictionary("me/noun/person.txt",word))            { return words[1][15];  }
-    if (findInDictionary("me/noun/quantity.txt",word))          { return words[1][16];  }
-    if (findInDictionary("me/noun/relation.txt",word))          { return words[1][17];  }
-    if (findInDictionary("me/noun/shape.txt",word))             { return words[1][18];  }
-    if (findInDictionary("me/noun/state.txt",word))             { return words[1][19];  }
-    if (findInDictionary("me/noun/substance.txt",word))         { return words[1][20];  }
-    if (findInDictionary("me/noun/tops.txt",word))              { return words[1][21];  }
+    if (findInDictionary("me/noun/act.text",word))              { return words[1][1];   }
+    if (findInDictionary("me/noun/animal.text",word))           { return words[1][2];   }
+    if (findInDictionary("me/noun/artifact.text",word))         { return words[1][3];   }
+    if (findInDictionary("me/noun/attribute.text",word))        { return words[1][4];   }
+    if (findInDictionary("me/noun/body.text",word))             { return words[1][5];   }
+    if (findInDictionary("me/noun/cognition.text",word))        { return words[1][6];   }
+    if (findInDictionary("me/noun/communication.text",word))    { return words[1][7];   }
+    if (findInDictionary("me/noun/event.text",word))            { return words[1][8];   }
+    if (findInDictionary("me/noun/feeling.text",word))          { return words[1][9];   }
+    if (findInDictionary("me/noun/food.text",word))             { return words[1][10];  }
+    if (findInDictionary("me/noun/group.text",word))            { return words[1][11];  }
+    if (findInDictionary("me/noun/location.text",word))         { return words[1][12];  }
+    if (findInDictionary("me/noun/motive.text",word))           { return words[1][13];  }
+    if (findInDictionary("me/noun/object.text",word))           { return words[1][14];  }
+    if (findInDictionary("me/noun/person.text",word))           { return words[1][15];  }
+    if (findInDictionary("me/noun/quantity.text",word))         { return words[1][16];  }
+    if (findInDictionary("me/noun/relation.text",word))         { return words[1][17];  }
+    if (findInDictionary("me/noun/shape.text",word))            { return words[1][18];  }
+    if (findInDictionary("me/noun/state.text",word))            { return words[1][19];  }
+    if (findInDictionary("me/noun/substance.text",word))        { return words[1][20];  }
+    if (findInDictionary("me/noun/tops.text",word))             { return words[1][21];  }
 
     // failing to find the word in any noun sub-categories, look in the main nouns list
-    if (findInDictionary("me/noun/noun.txt",word))              { return words[1][0];   }
+    if (findInDictionary("me/noun/noun.text",word))             { return words[1][0];   }
 
     // try to find the sub-category of the word if we assume it to be a verb
-    if (findInDictionary("me/verb/body.txt",word))              { return words[2][1];   }
-    if (findInDictionary("me/verb/change.txt",word))            { return words[2][2];   }
-    if (findInDictionary("me/verb/cognition.txt",word))         { return words[2][3];   }
-    if (findInDictionary("me/verb/communication.txt",word))     { return words[2][4];   }
-    if (findInDictionary("me/verb/competition.txt",word))       { return words[2][5];   }
-    if (findInDictionary("me/verb/consumption.txt",word))       { return words[2][6];   }
-    if (findInDictionary("me/verb/contact.txt",word))           { return words[2][7];   }
-    if (findInDictionary("me/verb/creation.txt",word))          { return words[2][8];   }
-    if (findInDictionary("me/verb/emotion.txt",word))           { return words[2][9];   }
-    if (findInDictionary("me/verb/framestext.txt",word))        { return words[2][10];  }
-    if (findInDictionary("me/verb/motion.txt",word))            { return words[2][11];  }
-    if (findInDictionary("me/verb/perception.txt",word))        { return words[2][12];  }
-    if (findInDictionary("me/verb/possession.txt",word))        { return words[2][13];  }
-    if (findInDictionary("me/verb/social.txt",word))            { return words[2][14];  }
-    if (findInDictionary("me/verb/stative.txt",word))           { return words[2][15];  }
-    if (findInDictionary("me/verb/weather.txt",word))           { return words[2][16];  }
+    if (findInDictionary("me/verb/body.text",word))             { return words[2][1];   }
+    if (findInDictionary("me/verb/change.text",word))           { return words[2][2];   }
+    if (findInDictionary("me/verb/cognition.text",word))        { return words[2][3];   }
+    if (findInDictionary("me/verb/communication.text",word))    { return words[2][4];   }
+    if (findInDictionary("me/verb/competition.text",word))      { return words[2][5];   }
+    if (findInDictionary("me/verb/consumption.text",word))      { return words[2][6];   }
+    if (findInDictionary("me/verb/contact.text",word))          { return words[2][7];   }
+    if (findInDictionary("me/verb/creation.text",word))         { return words[2][8];   }
+    if (findInDictionary("me/verb/emotion.text",word))          { return words[2][9];   }
+    if (findInDictionary("me/verb/framestext.text",word))       { return words[2][10];  }
+    if (findInDictionary("me/verb/motion.text",word))           { return words[2][11];  }
+    if (findInDictionary("me/verb/perception.text",word))       { return words[2][12];  }
+    if (findInDictionary("me/verb/possession.text",word))       { return words[2][13];  }
+    if (findInDictionary("me/verb/social.text",word))           { return words[2][14];  }
+    if (findInDictionary("me/verb/stative.text",word))          { return words[2][15];  }
+    if (findInDictionary("me/verb/weather.text",word))          { return words[2][16];  }
 
     // failing to find the word in the verb sub-categories, look in the main verbs list
-    if (findInDictionary("me/verb/verb.txt",word))              { return words[2][0];   }
+    if (findInDictionary("me/verb/verb.text",word))             { return words[2][0];   }
 
 
     return words[0][0];
