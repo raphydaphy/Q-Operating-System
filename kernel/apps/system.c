c3347b14757df342af9f3e318953ec7d0b2914f4
Plankp <plankp@outlook.com>
2015-12-09 16:57:10 -0500

    Quality checked on system.c

--- kernel/apps/system.c
+++ kernel/apps/system.c
@@ -4,41 +4,41 @@
 
 void systemHelp(string cmd)
 {
-    print("\nThe argument you entered, ",0x4F);
-    print(cmd,0x4F);
-    print(" was not found.",0x4F);
-	  print("\nShowing help for system:",white);
-    newline();
+    print("\nThe argument ", 0x4F);
+    print(cmd, 0x4F);
+    print(" was not found.", 0x4F);
+    println("\nShowing help for system:",white);
     print("\nSystem Utility\nCurrently only able to reboot and halt system.\nHard shutdown to power off.\n\nMenu:\n1. [reboot]\n2. [halt]\n3. [shutdown] ", white);
 }
 
 void system(string args)
 {
-	if (streql(splitArg(args, 1),"reboot"))
+    args = splitArg(args, 1);
+	if (streql(args, "reboot"))
     {
 		reboot();
     }
-	else if (streql(splitArg(args, 1),"shutdown"))
+	else if (streql(args, "shutdown"))
     {
         shutdown();
     }
-  else if (streql(splitArg(args, 1),"halt"))
+    else if (streql(args, "halt"))
     {
         halt();
     }
-  else if (streql(splitArg(args, 1),"about"))
+    else if (streql(args, "about"))
     {
-        print("\n Q Operating System \nVersion: 0.06 Alpha\n", 0xCF);
+        print("\n Q Operating System\nVersion: 0.06 Alpha\n", 0xCF);
         //You might wanna replace your slack name with your full name (Its in alphabetical order)
-        print("\nDevelopers:\n",white);
-        print("- barteks2x\n",white);
-        print("- Patrick van Duyse\n",white);
-        print("- Plankp T\n",white);
-        print("- raph\n",white);
-        print("- Stanislaus Krisna\n",white);//stnkrisna
-        print("- Talip Goeksu",white);
+        println("\nDevelopers:", white);
+        println("- barteks2x", white);
+        println("- Patrick van Duyse", white);
+        println("- Plankp T", white);
+        println("- raph", white);
+        println("- Stanislaus Krisna", white);//stnkrisna
+        print("- Talip Goeksu", white);
     }
-  else
+    else
     {
         systemHelp(args);
     }
