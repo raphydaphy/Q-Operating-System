b45ba11a0dfb207d97d75ebf222c9ff33d59ec65
Talip Göksu <telip007@icloud.com>
2015-12-14 20:06:13 +0100

    Create stdarg.h

---
+++ kernel/inc/stdarg.h
@@ -0,0 +1,19 @@
+#ifndef STDARG_H
+#define STDARG_H
+
+
+// ------------------------------------------------------------------------------------------------
+// stdarg.h
+// added by telip007, based on osdev repo in github
+// ------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_arg(v,l)   __builtin_va_arg(v,l)
+#define va_end(v)     __builtin_va_end(v)
+#define va_copy(d,s)  __builtin_va_copy(d,s)
+
+typedef __builtin_va_list va_list;
+
+#endif
