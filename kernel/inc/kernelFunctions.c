bc4a0caa9a98aa64de086d575931ec70a2e928e0
Plankp <plankp@outlook.com>
2015-12-14 07:02:42 -0500

    Added try catch and set offsetted jumps

--- kernel/inc/kernelFunctions.c
+++ kernel/inc/kernelFunctions.c
@@ -130,14 +130,18 @@ void launchShell()
         	        muld,           // 0.5 * 3
         	        pushd, 3, 0,    // Pushes 3.0
         	        divd,           // 1.5 / 3.0
-//        	        pushd, 0, 0,    // Pushes 0.0
-//        	        divd,           // 0.5 / 0.0
-//                  setl, 0,        // new label 0
+
+                    seto, 2, 9,     // new label (for div)
+                    tryl, 2,
+                    setl, 0,        // new label 0
+                    pushd, 0, 0,    // Pushes 0.0
+                    divd,           // 0.5 / 0.0
+
+                    clrs,           // Clears stack
                     pushi, 0,       // Pushes 0
                     pushi, 10,      // Pushes 10
                     raddi,          // 0 +...+10
 //                  jmpl, 0,        // jmp label 0
-//                  clrs,           // Clears stack
         	        EOS             // End of prog
     	        };
         	    invokeOp(&currentEnv, ops, false);
