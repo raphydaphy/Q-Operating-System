0792aff702dd4cef4865dca0e57f57dcf8b8e6fc
Raph Hennessy <raph.hennessy@gmail.com>
2015-12-18 16:47:11 +1100

    made key interrupts work in painter.c

--- kernel/apps/painter.c
+++ kernel/apps/painter.c
@@ -1,31 +1,100 @@
 #include "painter.h"
 
-void painter(string args)
+// for an easy way to exit the program using return;
+// I made it an int type function yay :)
+int painter(string args)
 {
-    UNUSED(args);
-
-    printch(getch(),red);
-
-    waitUntilKey(3, 0x15 /*Y*/, 0x31 /*N*/, 0x2E /*C*/);
-
-    bool going = true;
-    while (going)
+    if (streql(splitArg(args, 1), "-H"))
     {
-        // 0x01 : ESCAPE : Exit the progarm
-        //
-
-        int key = getKeycode() / KC_MAGIC_VAL;
-
-        printint(key,red);
-
-        switch (key)
+        print("\nPainter is a simple drawing app that dosen't draw... yet",red);
+    }
+    else if (streql(splitArg(args, 1),"-NEW"))
+    {
+        while (true)
         {
-            case 0x01:
-                going = false;
-                break;
-            default:
-                break;
+            int key = getKeycode() / KC_MAGIC_VAL;
+
+            print("\nKeyCode: ",black);
+            printint(key,yellow);
+
+            switch (key)
+            {
+                case 1:
+                    // Escape Pressed
+                    return 0;
+                    break;
+                case 72:
+                    // Up Arrow Pressed
+                    print("\nUp Arrow Pressed",green);
+                    break;
+                case -54:
+                    // Up Arrow Released
+                    print("\nUp Arrow Released",red);
+                    break;
+                case 80:
+                    // Down Arrow Pressed
+                    print("\nDown Arrow Pressed",green);
+                    break;
+                case -46:
+                    // Down Arrow Released
+                    print("\nDown Arrow Released",red);
+                    break;
+                case 75:
+                    // Left Arrow Pressed
+                    print("\nLeft Arrow Pressed",green);
+                    break;
+                case -51:
+                    // Left Arrow Released
+                    print("\nLeft Arrow Released",red);
+                    break;
+                case 77:
+                    // Right Arrow Pressed
+                    print("\nRight Arrow Pressed",green);
+                    break;
+                case -49:
+                    // Right Arrow Released
+                    print("\nRight Arrow Released",red);
+                    break;
+                case 57:
+                    // Spacebar Pressed
+                    print("\nSpacebar Pressed",green);
+                    break;
+                case -70:
+                    // Spacebar Released
+                    print("\nSpacebar Released",red);
+                    break;
+                default:
+                    break;
+            }
         }
     }
+    else if (streql(splitArg(args, 1),""))
+    {
+        while (true)
+        {
+            // 0x01     : ESCAPE    : Exit the progarm
+            // 0x4E     : NUMPAD 6  : Move Cursor Right
+            // 0x38     : SPACE BAR : Do Nothing Atm :)
+
+            int key = waitUntilKey(3, 0x01, 0x4E, 0x38);
+
+            printint(key,red);
+
+            switch (key)
+            {
+                case 0x01:
+                    return 0;
+                    break;
+                case 0x4E:
+                    print("OMG!",yellow);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    return 0;
 
 }
